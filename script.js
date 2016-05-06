fea805ec29769e0633fa2fbd46661cb51f26296e
Kelseigh Ingram <keaingra@indiana.edu>
2016-05-06 01:33:49 -0400

    Add ID to ul element, add more JavaScript and jQuery

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 $(document).ready(function(){
-  
+  $(".selector").menu({
+    position: {my: "left top", at: "right-5 top+5"}
+  });
+  $(".selector").menu({
+    role: listbox
+  });
+  $(".selector").menu("collapseAll", null, true);
+  $(".selector").menu("expand");
 });
